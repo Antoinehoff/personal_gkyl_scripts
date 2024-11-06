357ba4bbab7158ccd8cdab8e28745ab78181514c
Antoinehoff <antoine-hoffmann@hotmail.com>
2024-11-06 09:25:18 -0500

    add the new routines

--- python_utilities/utils/__init__.py
+++ python_utilities/utils/__init__.py
@@ -6,7 +6,8 @@ from .math_utils import func_time_ave, func_calc_norm_fluc, integral_xyz,\
     custom_meshgrid, integral_yz
 from .plot_utils import func_data_omp,get_1xt_diagram,make_2D_movie,\
     plot_1D_time_evolution,label, plot_2D_cut, plot_domain, plot_GBsource,\
-    plot_1D_time_avg, plot_volume_integral_vs_t, plot_1D, plot_GB_loss
+    plot_1D_time_avg, plot_volume_integral_vs_t, plot_1D, plot_GB_loss,\
+    get_figdatadict, plot_figdatadict
 # You can also define __all__ to control what gets imported with a wildcard import (*)
 __all__ = [
     'find_prefix', 
@@ -28,7 +29,9 @@ __all__ = [
     'plot_volume_integral_vs_t',
     'plot_1D',
     'check_latex_installed',
-    'plot_GB_loss'
+    'plot_GB_loss',
+    'get_figdatadict',
+    'plot_figdatadict'
 ]
 
 # You can also include some utility constants or helper functions here if needed.
