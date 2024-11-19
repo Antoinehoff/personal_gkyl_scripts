f2aa89124dc404ccd581e775ff0e22b18fcd22af
Antoinehoff <antoine-hoffmann@hotmail.com>
2024-11-19 16:01:43 -0500

    add load_figout accessible

--- python_utilities/utils/__init__.py
+++ python_utilities/utils/__init__.py
@@ -7,7 +7,7 @@ from .math_utils import func_time_ave, func_calc_norm_fluc, integral_xyz,\
 from .plot_utils import func_data_omp,get_1xt_diagram,make_2D_movie,\
     plot_1D_time_evolution,label, plot_2D_cut, plot_domain, plot_GBsource,\
     plot_1D_time_avg, plot_volume_integral_vs_t, plot_1D, plot_GB_loss,\
-    get_figdatadict, plot_figdatadict, save_figout, plot_integrated_moment
+    get_figdatadict, plot_figdatadict, save_figout, plot_integrated_moment, load_figout
 # You can also define __all__ to control what gets imported with a wildcard import (*)
 __all__ = [
     'find_prefix', 
@@ -33,7 +33,8 @@ __all__ = [
     'get_figdatadict',
     'plot_figdatadict',
     'save_figout',
-    'plot_integrated_moment'
+    'plot_integrated_moment',
+    'load_figout'
 ]
 
 # You can also include some utility constants or helper functions here if needed.
