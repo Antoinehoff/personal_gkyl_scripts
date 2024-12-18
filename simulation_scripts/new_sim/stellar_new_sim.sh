be6a03000207e118c340b1e400a2c23a26941689
Antoinehoff <antoine-hoffmann@hotmail.com>
2024-12-18 16:58:11 -0500

    add a makefile example for stellar and use it in the new_sim script

--- simulation_scripts/new_sim/stellar_new_sim.sh
+++ simulation_scripts/new_sim/stellar_new_sim.sh
@@ -73,7 +73,7 @@ e) submit.sh automatically make, copy the executable in /wk, go to /wk, submit t
 f) notes can (and should) be taken in the sim_log.txt to keep trace of the restart changes
 EOT
 cp $HOME/personal_gkyl_scripts/simulation_scripts/input_c/TCV_NT_tq_300eV_48x32x16x12x6_nuvg.c $new_dir/input.c
-cp $HOME/personal_gkyl_scripts/simulation_scripts/Makefile $new_dir
+cp $HOME/personal_gkyl_scripts/simulation_scripts/makefiles/stellar_makefile $new_dir/Makefile
 cp $HOME/personal_gkyl_scripts/simulation_scripts/submit/stellar_submit.sh $new_dir
 cp $HOME/personal_gkyl_scripts/example.ipynb $new_dir/$new_dir.ipynb
 mkdir $new_dir/wk
