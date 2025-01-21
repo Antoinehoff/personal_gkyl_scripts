ae0a15945839fd4e1bbb8388e1c9a37f1467acc2
Antoinehoff <antoine-hoffmann@hotmail.com>
2025-01-21 10:11:39 -0500

    remove the python_utilities folder as everything has been transfered to the pygkyl library.

--- python_utilities/classes/physparam.py
+++
@@ -1,8 +0,0 @@
-# PhysParam.py
-class PhysParam:
-    def __init__(self, eps0, eV, mp, me, B_axis):
-        self.eps0 = eps0      # Permittivity of free space
-        self.eV = eV          # Elementary charge (eV)
-        self.mp = mp          # Proton mass
-        self.me = me          # Electron mass
-        self.B_axis = B_axis  # Magnetic field strength
