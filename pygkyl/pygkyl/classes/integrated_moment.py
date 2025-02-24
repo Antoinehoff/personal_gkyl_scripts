72f0440d727368e1cb53ccae72482bf56a3440cd
Antoinehoff <antoine-hoffmann@hotmail.com>
2025-02-24 17:06:04 -0500

    add the capability to read and plot the boundary flux diagnostics

--- pygkyl/pygkyl/classes/integrated_moment.py
+++ pygkyl/pygkyl/classes/integrated_moment.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class IntegratedMoment:
     simulation = None
-    name = None
+    momname = None
     spec_s = None
     values = None
     receipe = None
@@ -13,10 +13,56 @@ class IntegratedMoment:
     symbol = None
     time = None
     momtype = None
+    bflux = False
+    direction = None
+    edge = None
+    fluxextension = ''
+    fluxname = ''
+    bflux_list = ''
 
     def __init__(self, simulation, name, load=True, ddt=False):
         self.simulation = simulation
-        self.name = name
+
+        # convention: write bflux_x_u_fieldname with x the direction (x or z) and u the edge (u or l)
+        # or bflux_alldir
+        nres = '' # residual name after removing boundary flux extensions
+        if 'bflux' in name:
+            self.bflux = True
+            self.fluxextension = 'bflux'
+            nres = name
+            nres = nres.replace('bflux_','')
+
+            if nres[0] in ['x','z']:
+                self.direction = nres[0]
+                nres = nres.replace(self.direction+'_','')
+                self.fluxextension += nres[0] + '_'
+                self.fluxname += 'radial' if self.direction == 'x' else 'parallel'
+                if nres[0] in ['u','l']:
+                    self.edge = nres[0]
+                    nres = nres.replace(self.edge+'_','')
+                    self.fluxextension += nres[0] + '_'
+                    self.fluxname = 'upper '+ self.fluxname if self.edge == 'u' else 'lower ' + self.fluxname
+                elif nres[0:5] == 'total':
+                    self.edge = ['u','l']
+                    self.fluxextension += 'total_'
+                    nres = nres.replace('total_','')
+            elif nres[0:5] == 'total':
+                self.direction = ['x','z']
+                self.fluxextension += 'total_'
+                nres = nres.replace('total_','')
+            self.fluxname += ' loss'
+
+            self.bflux_list = []
+            dl = self.direction if isinstance(self.direction,list) else [self.direction] 
+            el = self.edge if isinstance(self.edge,list) else [self.edge] 
+            for d_ in dl:
+                for e_ in el:
+                    edgestring = 'lower' if e_=='l' else 'upper'
+                    self.bflux_list.append('_'+'bflux_'+d_+edgestring)
+        else: self.bflux_list = ['']
+
+        self.momname = nres if nres else name
+
         if name[-1] == 'e': self.spec_s = 'elc'
         elif name[-1] == 'i': self.spec_s = 'ion'
         elif name[-3:] == 'tot': self.spec_s = ['elc','ion']
@@ -32,103 +78,103 @@ class IntegratedMoment:
         simulation = self.simulation
         spec_s = self.spec_s
         if self.momtype in ['BiMaxwellian','bimaxwellian']:
-            if self.name[:-1] in ['n']:
+            if self.momname[:-1] in ['n']:
                 def receipe(x): return x[:,0]
                 scale = 1.0
                 vunits = 'particles'
                 symbol = r'$\bar n_%s$'%spec_s[0]
-            elif self.name[:-1] in ['upar']:
+            elif self.momname[:-1] in ['upar']:
                 def receipe(x): return x[:,1]
                 scale = simulation.species[spec_s].m*simulation.species[spec_s].vt
                 vunits = ''
                 symbol = r'$\bar u_{\parallel %s}/v_{t %s}$'%(spec_s[0],spec_s[0])
-            elif self.name[:-1] in ['Tpar']:
+            elif self.momname[:-1] in ['Tpar']:
                 def receipe(x): return x[:,2]
                 scale = simulation.species[spec_s].m
                 vunits = 'eV'
                 symbol = r'$\bar T_{\parallel %s}$'%spec_s[0]
-            elif self.name[:-1] in ['Tperp']:
+            elif self.momname[:-1] in ['Tperp']:
                 def receipe(x): return x[:,3]
                 scale = simulation.species[spec_s].m
                 vunits = 'eV'
                 symbol = r'$\bar T_{\perp %s}$'%spec_s[0]
-            elif self.name[:-1] in ['T']:
+            elif self.momname[:-1] in ['T']:
                 def receipe(x): return 1/3*(x[:,2]+2*x[:,3])
                 scale = simulation.species[spec_s].m
                 vunits = 'eV'
                 symbol = r'$\bar T_{%s}$'%spec_s[0]
-            elif self.name[:-1] in ['W','Pow']:
+            elif self.momname[:-1] in ['W','Pow']:
                 def receipe(x): return 1/3*(x[:,2]+2*x[:,3])
                 scale = simulation.species[spec_s].m / 1e6                
                 vunits = 'MJ'
                 symbol = r'$W_{kin,%s}$'%spec_s[0]
-            elif self.name in ['Wtot']:
+            elif self.momname in ['Wtot']:
                 def receipe(x): return 1/3*(x[:,2]+2*x[:,3])
                 scale = [simulation.species[s].m / 1e6 for s in spec_s]
                 vunits = 'MJ'
                 symbol = r'$W_{kin,tot}$'
-            elif self.name in ['ntot']:
+            elif self.momname in ['ntot']:
                 def receipe(x): return x[:,0]
                 scale = [1.0 for s in spec_s]      
                 vunits = 'particles'
-                symbol = r'$\bar n_{tot}$'    
+                symbol = r'$\bar n_{tot}$'  
             else:
-                print(self.name + ' is not available as integrated moment.')
+                print(self.momname + ' is not available as integrated moment.')
                 print('The available fields are: ns, upars, Tpars, tperps, Ts, Ws, Wtot, ntot. (for s=i,e).')
                 raise ValueError('Provided fieldname is not available')
         elif self.momtype in ['Hamiltonian','hamiltonian']:
-            if self.name[:-1] in ['n']:
+            if self.momname[:-1] in ['n']:
                 def receipe(x): return x[:,0]
                 scale = 1.0
                 vunits = 'particles'
                 symbol = r'$\bar n_%s$'%spec_s[0]
-            elif self.name[:-1] in ['upar']:
+            elif self.momname[:-1] in ['upar']:
                 def receipe(x): return x[:,1]
                 scale = simulation.species[spec_s].vt
                 vunits = ''
                 symbol = r'$\bar u_{\parallel %s}/v_{t %s}$'%(spec_s[0],spec_s[0])
-            elif self.name[:-1] in ['W','H']:
+            elif self.momname[:-1] in ['W','H']:
                 def receipe(x): return x[:,2]
                 scale = 1.0 / 1e6                
                 vunits = 'MJ'
                 symbol = r'$H_{%s}$'%spec_s[0]
-            elif self.name in ['Wtot','Htot']:
+            elif self.momname in ['Wtot','Htot']:
                 def receipe(x): return x[:,2]
                 scale = [1.0 / 1e6 for s in spec_s]
                 vunits = 'MJ'
                 symbol = r'$H_{tot}$'
-            elif self.name in ['ntot']:
+            elif self.momname in ['ntot']:
                 def receipe(x): return x[:,0]
                 scale = [1.0 for s in spec_s]      
                 vunits = 'particles'
                 symbol = r'$\bar n_{tot}$'    
             else:
-                print(self.name + ' is not available as integrated moment.')
+                print(self.momname + ' is not available as integrated moment.')
                 print('The available fields are: ns, upars, Tpars, tperps, Ts, Ws, Wtot, ntot. (for s=i,e).')
                 raise ValueError('Provided fieldname is not available')
         else:
             print(self.momtype+' not available. Must be BiMaxwellian or Hamiltonian')
             raise ValueError('Provided momtype is not available')
         self.receipe = receipe
-        self.scale = scale
+        self.scale = scale if isinstance(scale,list) else [scale]
         self.vunits = vunits
         self.tunits = self.simulation.normalization.dict['tunits']
-        self.symbol = symbol    
+        self.symbol = symbol
+        if self.bflux:
+            self.symbol += ' '+self.fluxname
+            self.vunits += '/s' if self.vunits else '1/s'
 
     def load(self):
         """
         Load the integrated moment data from the simulation.
         """
         self.values = 0
-        if isinstance(self.spec_s,str):
-            f_ = self.simulation.data_param.fileprefix+'-'+self.spec_s+'_integrated_moms.gkyl'
-            Gdata = pgkyl_.get_gkyl_data(f_)
-            self.values = pgkyl_.get_values(Gdata) * self.scale
-        else:
-            for s in self.spec_s:
-                f_ = self.simulation.data_param.fileprefix+'-'+s+'_integrated_moms.gkyl'
+        species_list = self.spec_s if isinstance(self.spec_s,list) else [self.spec_s]
+        for s_ in species_list:
+            for bf_ in self.bflux_list:
+                f_ = self.simulation.data_param.fileprefix+'-'+s_+bf_+'_integrated_moms.gkyl'
                 Gdata = pgkyl_.get_gkyl_data(f_)
-                self.values += pgkyl_.get_values(Gdata) * self.scale[self.spec_s.index(s)]
+                self.values += pgkyl_.get_values(Gdata) * self.scale[species_list.index(s_)]
 
         self.time = np.squeeze(Gdata.get_grid()) / self.simulation.normalization.dict['tscale']
         self.values = self.receipe(self.values)
