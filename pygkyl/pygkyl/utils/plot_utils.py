abefa4f88986ddd6549d7ce51c9ce2b699ffb7fd
Antoinehoff <antoine-hoffmann@hotmail.com>
2025-05-01 15:33:23 -0400

    Fix colormap reference in plot_2D_cut to use field_info_dict instead of dict

--- pygkyl/pygkyl/utils/plot_utils.py
+++ pygkyl/pygkyl/utils/plot_utils.py
@@ -184,7 +184,7 @@ def plot_2D_cut(simulation, cut_dir, cut_coord, time_frame,
                 plot_data = frame.values
             del serie
         
-        if ((cmap0 == 'bwr' or (simulation.data_param.dict[field+'colormap']=='bwr') \
+        if ((cmap0 == 'bwr' or (simulation.data_param.field_info_dict[field+'colormap']=='bwr') \
             or fluctuation) and not fourier_y) :
             cmap = 'bwr'
             vmax = np.max(np.abs(plot_data)) 
