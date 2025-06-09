294adb859fd91f85cd417b564bc65f0bfafc340a
Antoinehoff <antoine-hoffmann@hotmail.com>
2025-06-09 16:38:48 -0400

    fix: the function naming of gradlog

--- pygkyl/pygkyl/classes/dataparam.py
+++ pygkyl/pygkyl/classes/dataparam.py
@@ -377,22 +377,24 @@ class DataParam:
             symbol     = r'$-\nabla \ln n_{%s}$'%(s_)
             units      = '1/m'
             field2load = ['n%s'%(s_)]
-            def receipe_ntots(gdata_list):
+            def receipe_gradn(gdata_list):
                 dens = pgkyl_.get_values(gdata_list[0])
+                dens[dens <= 0] = np.nan # avoid log(0)
                 grids = gdata_list[0].get_grid()
                 return -np.gradient(np.log(dens), grids[0][:-1], axis=0)
-            default_qttes.append([name,symbol,units,field2load,receipe_ntots])
+            default_qttes.append([name,symbol,units,field2load,receipe_gradn])
 
             #normalized radial temperature gradient
             name       = 'gradlogT%s'%(s_)
             symbol     = r'-\nabla \ln T_{%s}$'%(s_)
             units      = '1/m'
             field2load = ['Tpar%s'%(s_),'Tperp%s'%(s_)]
-            def receipe_Ttots(gdata_list):
-                logT = np.log((pgkyl_.get_values(gdata_list[0]) + 2.0*pgkyl_.get_values(gdata_list[1]))/3.0)
+            def receipe_gradT(gdata_list):
+                temp = pgkyl_.get_values(gdata_list[0]) + 2.0*pgkyl_.get_values(gdata_list[1])/3.0
+                temp[temp <= 0] = np.nan # avoid log(0)
                 grids = gdata_list[0].get_grid()
-                return -np.gradient(logT, grids[0][:-1], axis=0)
-            default_qttes.append([name,symbol,units,field2load,receipe_Ttots])
+                return -np.gradient(np.log(temp), grids[0][:-1], axis=0)
+            default_qttes.append([name,symbol,units,field2load,receipe_gradT])
 
             #Kinetic energy density speciewise: Wkins = int dv3 1/2 ms vpar^2 + mus B
             name       = 'Wkin%s'%(s_)
