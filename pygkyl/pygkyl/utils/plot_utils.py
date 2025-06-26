add the units and fix /4 integer division

pygkyl/pygkyl/utils/plot_utils.py
@@ -732,29 +732,30 @@ def plot_balance(simulation, balancetype='particle', title=True, figout=[]):
             intmom = IntegratedMoment(simulation=simulation, name=fieldname, load=True, ddt=False)
         except KeyError:
             raise ValueError(f"Cannot find field '{fieldname}' in the simulation data. ")
-        return intmom.values, intmom.time
+        return intmom.values, intmom.time, intmom.vunits, intmom.tunits
     
     fieldname = 'src_ntot' if balancetype == 'particle' else 'src_Htot'
-    source, time = get_int_mom_data(simulation, fieldname)
+    source, time, vunits, tunits = get_int_mom_data(simulation, fieldname)
     
     fieldname = 'Wtot' if balancetype == 'energy' else 'ntot'
-    intvar, time = get_int_mom_data(simulation, fieldname)
+    intvar, time, vunits, tunits = get_int_mom_data(simulation, fieldname)
     
     fieldname = 'bflux_total_total_ntot' if balancetype == 'particle' else 'bflux_total_total_Htot'
-    loss, time = get_int_mom_data(simulation, fieldname)
+    loss, time, vunits, tunits = get_int_mom_data(simulation, fieldname)
     
     balance = source - loss - intvar
     
     nt = len(time)
-    balance_avg = np.mean(balance[-nt/4:])
+    balance_avg = np.mean(balance[-nt//4:])
         
     fig, ax = plt.subplots(figsize=(fig_tools.default_figsz[0], fig_tools.default_figsz[1]))
     ax.plot(time, balance, label='Balance')
     # Add horizontal line at average balance value
     ax.axhline(y=balance_avg, color='gray', linestyle='--', label='Average Balance: {:.2e}'.format(balance_avg))
-    xlabel = r'$t$ [%s]' % intmom.tunits
+    xlabel = r'$t$ [%s]' % tunits if  tunits else r'$t$'
     ylabel = r'$\Gamma_{\text{src}} - \Gamma_{\text{loss}} - \partial N / \partial t$' if  balancetype == 'particle' else \
              r'$P_{\text{src}} - P_{\text{loss}} - \partial H / \partial t$'
+    ylabel += ' [%s]' % vunits if vunits else ''
     title_ = f'%s Balance' % balancetype.capitalize() if  title else ''
         
     fig_tools.finalize_plot(ax, fig, xlabel=xlabel, ylabel=ylabel, figout=figout,