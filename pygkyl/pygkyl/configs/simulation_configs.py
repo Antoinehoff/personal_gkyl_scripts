7b14df911e675f5f950dc3a85a9da66f7d08d88e
Antoinehoff <antoine-hoffmann@hotmail.com>
2025-12-18 11:21:01 -0800

    switch the arguments back to allow for retrocompatibility

--- pygkyl/pygkyl/configs/simulation_configs.py
+++ pygkyl/pygkyl/configs/simulation_configs.py
@@ -5,7 +5,7 @@ from ..interfaces.gyacomointerface import get_gyacomo_sim_config
 from ..interfaces.pgkyl_interface import get_dimensionality
 from .vessel_data import tcv_vessel_data, d3d_vessel_data, sparc_vessel_data, nstxu_vessel_data
 
-def import_config(simDir, filePrefix = '', configName='tcv_pt', x_LCFS = None, x_out = None, 
+def import_config(configName='tcv_pt', simDir ='', filePrefix = '', x_LCFS = None, x_out = None, 
                   load_metric=True, add_source=True, dimensionality=None, simidx=0):
     if dimensionality is None:
         dimensionality = get_dimensionality(simDir, filePrefix)
